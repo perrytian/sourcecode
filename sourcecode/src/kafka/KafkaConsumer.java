a2935baa2cf51b56b81f5c3f7d3a014d4274534c
18601105586@163.com <18601105586@163.com>
2015-08-19 16:06:54 +0800

    提交新代码Kafka

---
+++ sourcecode/src/kafka/KafkaConsumer.java
@@ -0,0 +1,83 @@
+package kafka;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Properties;
+
+import kafka.consumer.Consumer;
+import kafka.consumer.ConsumerConfig;
+import kafka.consumer.ConsumerIterator;
+import kafka.consumer.KafkaStream;
+import kafka.javaapi.consumer.ConsumerConnector;
+import kafka.message.MessageAndMetadata;
+
+public class KafkaConsumer {
+
+	ConsumerConnector consumerConn ;
+	
+	KafkaStream<byte[], byte[]> kafkaStream = null;
+	
+	ConsumerIterator<byte[], byte[]> consumerIter = null;
+	
+	String zookeeper;
+	String groupId ;
+	String topic;
+	
+	/**
+	 * 
+	 * @param zookeeper "192.168.1.1:2181,192.168.1.2:2181"
+	 */
+	public KafkaConsumer(String zookeeper, String groupId, String topic) {
+		this.zookeeper = zookeeper;
+		this.groupId = groupId;
+		this.topic = topic;
+		
+		init();
+	}
+	
+	public void init(){
+		
+		Properties props = new Properties();
+		props.put("zookeeper.connect", zookeeper);
+		props.put("group.id", groupId);
+		props.put("auto.offset.reset", "smallest");
+		props.put("zookeeper.session.timeout.ms", "400");
+		props.put("zookeeper.sync.time.ms", "2000");
+		props.put("auto.commit.interval.ms", "1000");
+
+		ConsumerConfig consumerConfig = new ConsumerConfig(props);
+
+		Map<String, Integer> topicCountMap = new HashMap<String, Integer>();
+		topicCountMap.put(topic, new Integer(1));
+		
+		consumerConn = Consumer.createJavaConsumerConnector( consumerConfig );
+		
+		Map<String, List<KafkaStream<byte[], byte[]>>> consumerMap = consumerConn
+				.createMessageStreams(topicCountMap);
+		
+		List<KafkaStream<byte[], byte[]>> streams = consumerMap.get(topic);
+		
+		kafkaStream = streams.get(0);
+		
+		
+		consumerIter = kafkaStream.iterator();
+	}
+
+	public boolean hasNext(){
+		return consumerIter.hasNext();
+	}
+	
+	public MessageAndMetadata<byte[], byte[]> next(){
+		return consumerIter.next();
+	}
+	
+	public byte[] getNext(){
+		byte[] ret = null ;
+		if( consumerIter.hasNext() ){
+			ret = consumerIter.next().message();
+		}
+		return ret;
+	}
+	
+}
---
+++ sourcecode/src/kafka/KafkaProducer.java
@@ -0,0 +1,69 @@
+package kafka;
+import java.net.ConnectException;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Properties;
+
+import kafka.javaapi.producer.Producer;
+import kafka.producer.KeyedMessage;
+import kafka.producer.ProducerConfig;
+
+public class KafkaProducer {
+
+	String brokers = null;
+	String topic = null;
+
+	Producer<Integer, String> producer;
+	private final Properties props = new Properties();
+
+	public int partitionMethod = -1; // random
+
+	public KafkaProducer(String brokers) {
+		this(brokers, null);
+	}
+
+	/**
+	 * 
+	 * @param brokers
+	 *            "192.168.1.6:9092,192.168.1.7:9092,192.168.1.8:9092,192.168.1.8:9092,192.168.1.9:9092"
+	 * @param topic
+	 */
+	public KafkaProducer(String brokers, String topic) {
+		this.brokers = brokers;
+		this.topic = topic;
+		init();
+	}
+	
+	public void init(){
+		props.put("serializer.class", "kafka.serializer.StringEncoder");
+		props.put("metadata.broker.list", brokers);
+		props.put("partitioner.key", String.valueOf(partitionMethod));
+		props.put("partitioner.class", "com.cucc.cdrmonitor.kafka.SimplePartitioner");
+
+		producer = new Producer<Integer, String>(new ProducerConfig(props));
+	}
+
+	public void send(String content) throws Exception{
+		KeyedMessage<Integer, String> message = new KeyedMessage<Integer, String>(
+				topic, content);
+		producer.send(message);	
+	}
+
+	public void send(List<String> contents) throws Exception {
+		List<KeyedMessage<Integer, String>> ms = new LinkedList<KeyedMessage<Integer, String>>();
+		for (String i : contents) {
+			ms.add(new KeyedMessage<Integer, String>(topic, i));
+		}
+		producer.send(ms);
+	}
+	
+	
+	public void close(){
+		try{
+			producer.close();
+		} catch(Exception e){
+			
+		}
+	}
+
+}
